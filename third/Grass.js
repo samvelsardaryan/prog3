3ae0828ec5a01e18551836ff07bbf217862415e1
Samvel Sardaryan <samvel.sardaryan.y@tumo.org>
2021-10-03 00:19:05 +0400

    start of the end

---
+++ third/Grass.js
@@ -0,0 +1,35 @@
+let LivingCreature = require('./LivingCreature')
+
+module.exports = class Grass extends LivingCreature {
+
+    mul() {
+        this.multiply++;
+        if (this.multiply >= 5) {
+            let emptyCells = super.chooseCell(0)
+            let newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+            if (this.multiply >= 5 && newCell) {
+                let x = newCell[0]
+                let y = newCell[1]
+                matrix[y][x] = 1
+                grassArr.push(new Grass(x, y, 1))
+                this.multiply = 0;
+            }
+            if (weath == "winter") {
+                this.energy -= 2;
+                this.multiply -= 2;
+            }
+            if (weath == "spring") {
+                this.energy += 5;
+                this.multiply += 5;
+            }
+            if (weath == "summer") {
+                this.energy += 3;
+                this.multiply += 3;
+            }
+            if (weath == "autumn") {
+                this.energy--;
+                this.multiply--;
+        }
+    }
+}
+}
---
+++ third/script.js
@@ -0,0 +1,49 @@
+var socket = io();
+
+var side = 10;
+function setup() {
+    createCanvas(50 * side, 50 * side);
+    background("pink");
+}
+
+socket.on("weather", function (data) {
+    weath = data;
+})
+
+function nkarel(matrix) {
+    for (var y = 0; y < matrix.length; y++) {
+        for (var x = 0; x < matrix[0].length; x++) {
+            var obj = matrix[y][x];
+            if (obj == 1){
+                if(weath == "summer") {
+                fill("green");
+            }else if (weath == "autumn") {
+                fill("#333300");
+            }else if (weath == "winter") {
+                fill("white");
+            }else if (weath == "spring") {
+                fill("#4dffa6");
+            }
+        }else if (obj == 2) {
+                fill("yellow");
+            }else if (obj == 0){
+                fill("grey")
+            }
+            rect(x * side, y * side, side, side);
+        }
+    }
+}
+
+        socket.on('send matrix', nkarel)
+ 
+
+
+function kill() {
+    socket.emit("kill")
+}
+function addGrass() {
+    socket.emit("add grass")
+}
+function addGrassEater() {
+    socket.emit("add grassEater")
+}
